fb35ea105a535c21372957f19e75f83783e1ecd6
Anurag Mohanty <tevren@gmail.com>
2014-06-26 15:11:41 -0400

    play211 mirrored on s3 now

--- play21.rb
+++ play21.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Play21 < Formula
   homepage 'http://www.playframework.org/'
   head 'https://github.com/playframework/playframework.git'
-  url 'http://downloads.typesafe.com/play/2.1.1/play-2.1.1.zip'
+  url 'http://s3.amazonaws.com/amplify-shared/play-2.1.1.zip'
   sha1 '3b1a0c3ca23dde36113d935b85d7f4d45f05923b'
 
   conflicts_with 'sox', :because => 'both install `play` binaries'
